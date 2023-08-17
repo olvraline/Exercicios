83892e93b700faf2c80f29abc016e3351b01028c
Aline Oliveira <aline.t_oliveira@hotmail.com>
2023-08-17 14:52:15 -0300

    Exercicios condicoes 5

---
+++ ExercicioCondicoes5/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Digite seu nome");
+string nome = Console.ReadLine();
+
+Console.WriteLine("Digite sua idade");
+int idade = int.Parse(Console.ReadLine());
+
+if (idade <=12)
+{
+    Console.WriteLine($"Bem vindo, {nome}");
+}
+else if (idade >=13  && idade <= 19)
+{
+    Console.WriteLine($"Olá,{nome}");
+}
+else
+{
+    Console.WriteLine($"Olá Sr./Sra. {nome}");
+}
